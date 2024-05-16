997cdb3a8c24b43e69e33242b323a976ae4da40d
Martin-Moras <martin.moras@web.de>
2024-05-16 10:47:44 +0200

    added connections, nodes and started with connection text

---
+++ Assets/scripts/Connection.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Connection : MonoBehaviour
+{
+	[SerializeField] Transform inputNode;
+    [SerializeField] Transform outputNode;
+	TextMeshPro text;
+	SpringJoint2D joint;
+    LineRenderer connectionRenderer;
+
+
+    void Start()
+    {
+        initializeVariables();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        manageJoint();
+        updatePos();
+    }
+   
+    private void updatePos(){
+        Vector3[] positions = { joint.transform.position, joint.connectedBody.position };
+        connectionRenderer.SetPositions(positions);
+    }
+    private void manageJoint(){
+        if(joint == null){
+            joint = inputNode.AddComponent<SpringJoint2D>();
+            joint.connectedBody = outputNode.GetComponent<Rigidbody2D>();
+            joint.autoConfigureDistance = false;
+            joint.distance = 3;
+        }
+    }
+	private void initializeVariables(){
+		connectionRenderer = GetComponent<LineRenderer>();
+		//Text
+		text = gameObject.AddComponent<TextMeshPro>();
+		text.fontSize = 5;
+		text.alignment = TextAlignmentOptions.Midline;
+	}
+}
---
+++ Assets/scripts/Node.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    List<Connection> connections;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+/*
+connected node
+connection info (name, color ...)
+
+*/
